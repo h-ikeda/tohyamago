b3c251f4918f18b7bd72391e4a1bcd923adb4f03
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2024-10-22 04:07:23 +0000

    フッターの視覚効果と装飾の調整

--- public/App.vue
+++ public/App.vue
@@ -30,10 +30,12 @@ body {
 <style scoped>
 main {
   @apply grow overflow-auto;
-  @apply after:sticky after:bottom-0 after:bg-gradient-to-t after:from-gray-100 after:to-transparent after:w-full after:h-24 after:block;
+  @apply after:sticky after:bottom-0 after:bg-gradient-to-t after:from-gray-100 after:to-transparent after:via-gray-100 after:opacity-90 after:w-full after:h-20 after:block;
 }
 
 footer {
-  @apply text-center text-sm py-1 border-t border-lime-200 bg-lime-100 text-gray-700 font-light;
+  @apply text-center text-sm py-1 border-t border-lime-200 bg-lime-100 text-gray-700 font-light relative -z-10;
+  @apply before:border-x-[0.58rem] before:border-b-[1rem] before:border-x-transparent before:border-b-green-500 before:absolute before:right-4 before:bottom-2;
+  @apply after:border-x-[0.58rem] after:border-b-[1rem] after:border-x-transparent after:border-b-green-700 after:absolute after:right-2 after:bottom-1 after:scale-110 after:origin-bottom-right;
 }
 </style>
