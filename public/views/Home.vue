207ecea8f58f9bf5424234644d22851b33d85437
h-ikeda <ikeda_hiroki@icloud.com>
2025-09-08 15:00:23 +0000

    tailwindcss migrations (v3 to v4)

--- public/views/Home.vue
+++ public/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>一般社団法人遠山郷応援会</h1>
+  <h1 class="w-fit mx-auto text-xl my-8 relative
+             before:border-x-[1rem] before:border-x-transparent before:border-b-[1.73rem] before:border-b-green-500 before:relative before:-left-3.5 before:-top-7 before:scale-110
+             after:border-x-[1rem] after:border-x-transparent after:border-b-[1.73rem] after:border-b-green-700 after:absolute after:-left-1 after:top-0 after:scale-[1.35]"
+  >
+    一般社団法人遠山郷応援会
+  </h1>
   <section class="w-fit mx-auto my-6">
     <img
       class="aspect-video object-cover"
@@ -35,11 +40,3 @@ const { hash } = defineProps<{
   hash?: string
 }>()
 </script>
-
-<style scoped>
-h1 {
-  @apply w-fit mx-auto text-xl my-8 relative;
-  @apply before:border-x-[1rem] before:border-x-transparent before:border-b-[1.73rem] before:border-b-green-500 before:relative before:-left-3.5 before:-top-7 before:scale-110;
-  @apply after:border-x-[1rem] after:border-x-transparent after:border-b-[1.73rem] after:border-b-green-700 after:absolute after:-left-1 after:top-0 after:scale-[1.35];
-}
-</style>
