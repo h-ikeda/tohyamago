2a055e5956d4a8d00d7ec55a96b61b72ef117728
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2025-09-11 00:18:18 +0900

    予定タブのコンテンツを追加 (#67)

--- public/views/Home.vue
+++ public/views/Home.vue
@@ -11,23 +11,35 @@
       :src="farmImage.toString()"
     >
   </section>
-  <nav class="flex text-lg font-medium justify-center mb-2 gap-2">
+  <nav class="flex text-lg font-medium justify-center mb-4 gap-2 border-b border-gray-500 shadow-md">
     <RouterLink
-      class="tracking-widest border-black px-10 bg-white rounded-t-md"
-      :class="{'border-b-2': hash !== '#events', 'text-gray-600': hash === '#events'}"
+      class="tracking-widest px-10 bg-white rounded-t-md border-x border-t border-gray-500"
+      :class="{'border-b-2': hash !== '#events', 'text-gray-500': hash === '#events'}"
       to="#feed"
     >
       近況
     </RouterLink>
     <RouterLink
-      class="tracking-widest px-10 border-black bg-white rounded-t-md pointer-events-none"
-      :class="{'border-b-2': hash === '#events', 'text-gray-600': hash !== '#events'}"
+      class="tracking-widest px-10 border-black bg-white rounded-t-md border-x border-t"
+      :class="{'border-b-2': hash === '#events', 'text-gray-500': hash !== '#events'}"
       to="#events"
     >
       予定
     </RouterLink>
   </nav>
-  <FacebookFeed v-if="hash === '#events'"/>
+  <section v-if="hash === '#events'">
+    <p class="mx-5 bg-white border px-6 py-4 rounded-xl tracking-tighter">
+      今後の活動予定は、<a class="underline" href="https://activo.jp/s/a/119414">ボランティア募集ページ</a>に掲載しています。「所属期間/頻度」欄をご確認下さい。
+    </p>
+    <p class="mx-auto w-fit font-medium text-lime-600 text-lg">
+      <a
+        class="border-3 bg-lime-100 px-4 py-2 my-4 block border-double shadow-md flex items-center gap-2
+               after:border-l-10 after:border-y-6 after:border-y-transparent after:block after:h-0"
+        href="https://activo.jp/s/a/119414">
+        ボランティア募集中!
+      </a>
+    </p>
+  </section>
   <facebookFeed v-else/>
 </template>
 
